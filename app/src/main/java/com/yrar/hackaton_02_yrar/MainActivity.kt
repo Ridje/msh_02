aa389f09caa710e3b7e23cd3e59ddebe6e806f02
ridje <angelnumber59@gmail.com>
2021-06-11 20:11:58 +0300

    initial commit

---
+++ app/src/main/java/com/yrar/hackaton_02_yrar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yrar.hackaton_02_yrar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
