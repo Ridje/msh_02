5ecb2c5e22c4977f2b85cb8b160f4f98c25dd792
ridje <angelnumber59@gmail.com>
2021-06-13 01:38:48 +0300

    Added buttons share and add to calendar

--- app/src/main/java/com/yrar/hackaton_02_yrar/ui/event/EventFragment.kt
+++ app/src/main/java/com/yrar/hackaton_02_yrar/ui/event/EventFragment.kt
@@ -3,6 +3,8 @@ package com.yrar.hackaton_02_yrar.ui.event
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events
 import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +15,8 @@ import com.yrar.hackaton_02_yrar.MainActivity
 import com.yrar.hackaton_02_yrar.databinding.EventFragmentBinding
 import com.yrar.hackaton_02_yrar.model.app.Event
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
 
 private const val EVENT_ARGUMENT_KEY = "EVENT_OPENED"
 
@@ -49,10 +53,55 @@ class EventFragment : Fragment() {
             (requireActivity() as MainActivity).navigateToDefaultFragment()
         }
         binding.likeIcon.setOnClickListener { onLikeClick(view) }
+        binding.buttonShare.setOnClickListener {
+            shareContent()
+        }
+        binding.buttonAddToCalendar.setOnClickListener {
+            addToCalendar()
+        }
 
         (requireActivity() as MainActivity).setBottomNavigationMenuVisibility(false)
 //        binding.contactEmail.setOnClickListener { onContactEmailClick(view) }
 //        binding.webAddress.setOnClickListener{ onWebClick(view) }
+    }
+
+    fun addToCalendar() {
+        event?.let {
+            val intent: Intent = Intent(Intent.ACTION_INSERT)
+                .setData(Events.CONTENT_URI)
+                .putExtra(
+                    CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                    it.dateStart
+                )
+
+            when {
+                (event?.dateEnd != null) -> intent.putExtra(
+                    CalendarContract.EXTRA_EVENT_END_TIME,
+                    it.dateEnd
+                )
+            }
+
+            intent.putExtra(Events.TITLE, it.title)
+                .putExtra(Events.DESCRIPTION, it.type + " " + it.description)
+                .putExtra(Events.EVENT_LOCATION, it.address)
+                .putExtra(Events.AVAILABILITY, Events.AVAILABILITY_BUSY)
+
+            startActivity(intent)
+        }
+    }
+
+    fun shareContent() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT,
+                event?.title + "\n" + event?.description + "\n" + event?.type + " " + DateFormat.getDateFormat(context)
+                    .format(event?.dateStart) + "\n" + "https://i.moscow/event/${event?.id}"
+            )
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 
     fun onLikeClick(view: View) {
